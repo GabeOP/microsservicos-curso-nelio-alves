251c3d9f717f2e865d6d71c10a03681837f02840
GabeOP <gabrieloliveirap1@hotmail.com>
2023-08-17 11:57:46 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/com/gabriel/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/gabriel/hrworker/resources/WorkerResource.java
@@ -1,10 +1,11 @@
 package com.gabriel.hrworker.resources;
 
-import java.util.List;
-
+import com.gabriel.hrworker.entities.Worker;
+import com.gabriel.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +13,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gabriel.hrworker.entities.Worker;
-import com.gabriel.hrworker.repositories.WorkerRepository;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+	@Value("${test.config}")
+	private String testConfig;
 	
 	@Autowired
 	private Environment env;
@@ -40,5 +43,10 @@ public class WorkerResource {
 		
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
+	}
+
+	@GetMapping(value = "/configs")
+	public ResponseEntity<String> getConfigs() {
+		return ResponseEntity.ok().body("CONFIGURACAO= " + testConfig);
 	}
 }
